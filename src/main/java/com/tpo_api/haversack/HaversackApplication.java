d94652ba241ac68bf79c6c7e0074e1d06579abac
tsamaan <tsamaanpiroli@uade.edu.ar>
2025-10-01 18:16:37 -0300

    inicializacion del proyecto y archivos md

---
+++ src/main/java/com/tpo_api/haversack/HaversackApplication.java
@@ -0,0 +1,13 @@
+package com.tpo_api.haversack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaversackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaversackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpo_api/haversack/HaversackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpo_api.haversack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaversackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
